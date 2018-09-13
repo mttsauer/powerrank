508438045615f67036ac1ff7b64ae0a618ea1bf2
Matt Sauer <MattESauer@gillettechildrens.com>
2018-09-13 17:29:01 -0500

    Added season recursion for record, points, and breakdown.  Also avg rank

--- routes/powerrank.js
+++ routes/powerrank.js
@@ -5,140 +5,130 @@ const asyncHandler = require("express-async-handler");
 
 var router = express.Router();
 
-router.post('/', function(req, res, next) {
-  res.redirect('/powerrank/'+req.body.leagueId+'/2018/1');
+router.post('/', function (req, res, next) {
+  res.redirect('/powerrank/' + req.body.leagueId + '/2018/1');
 });
 
 /* GET users listing. */
-router.get(
-  "/:leagueId/:seasonId/:matchupPeriodId",
-  asyncHandler(async (req, res, next) => {
-    var teams = {};
-    superagent
-      .get(
-        generateUrl(
-          req.params.leagueId,
-          req.params.seasonId,
-          req.params.matchupPeriodId
-        )
+router.get("/:leagueId/:seasonId/:matchupPeriodId", function (req, res, next) {
+  var teams = {};
+  superagent
+    .get(
+      generateUrl(
+        req.params.leagueId,
+        req.params.seasonId,
+        req.params.matchupPeriodId
       )
-      .end((err, response) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        matchupPeriod = jsonQuery("scoreboard.matchupPeriodId", {
-          data: response.body
-        }).value;
+    )
+    .end((err, response) => {
+      if (err) {
+        console.log(err);
+        res.send(err);
+        return;
+      }
+      matchupPeriod = jsonQuery("scoreboard.matchupPeriodId", {
+        data: response.body
+      }).value;
 
-        teams = jsonQuery("scoreboard.matchups.teams", { data: response.body })
-          .value;
+      teams = jsonQuery("scoreboard.matchups.teams", { data: response.body })
+        .value;
 
-        points = new Array();
-        records = new Array();
-        breakdowns = new Array();
+      points = new Array();
+      records = new Array();
+      breakdowns = new Array();
+      teamList = new Array();
 
-        teams.forEach(team => {
-          points.push({
-            teamId: team.teamId,
-            name: team.team.teamLocation + " " + team.team.teamNickname,
-            compare: function() {
-              return team.team.record.pointsFor;
-            },
-            display: function() {
-              return team.team.record.pointsFor;
-            }
-          });
-          records.push({
-            teamId: team.teamId,
-            name: team.team.teamLocation + " " + team.team.teamNickname,
-            compare: function() {
-              return (
-                (team.team.record.overallWins || 0) -
-                (team.team.record.overallLosses || 0)
-              );
-            },
-            display: function() {
-              return (
-                (team.team.record.overallWins || 0) +
-                "-" +
-                (team.team.record.overallLosses || 0)
-              );
-            }
-          });
-        });
-
-        //Replace with async call to all previous periods
-        breakdowns = calculateBreakdown(teams);
-        //breakdowns = getBreakdowns(matchupPeriod, req.params.leagueId, req.params.seasonId)
-
-        rank(breakdowns);
-        rank(points);
-        rank(records);
-
-        ranks = new Array();
-        points.forEach(pointElement => {
-          breakdownElement =
-            jsonQuery("[teamId=" + pointElement.teamId + "]", {
-              data: breakdowns
-            }).value || {};
-          recordElement = jsonQuery("[teamId=" + pointElement.teamId + "]", {
-            data: records
-          }).value;
-          ranks.push({
-            name: pointElement.name,
-            points: pointElement.display(),
-            pointsRank: points.length + 1 - pointElement.rank,
-            breakdown: breakdownElement.display(),
-            breakdownRank: points.length + 1 - breakdownElement.rank,
-            record: recordElement.display(),
-            recordRank: points.length + 1 - recordElement.rank,
-            powerPoints:
-              points.length +
-              1 -
-              pointElement.rank +
-              points.length +
-              1 -
-              breakdownElement.rank +
-              points.length +
-              1 -
-              recordElement.rank,
-            compare: function() {
-              return this.powerPoints;
-            }
-          });
-        });
-
-        rank(ranks);
-        res.render("powerrank", { ranks: ranks });
+      //Grab the current name as of the period
+      teams.forEach(team => {
+        teamList.push({teamId: team.teamId, name: team.team.teamLocation + " " + team.team.teamNickname});
       });
-  })
-);
 
-rank = function(data = {}) {
+      getData(req.params.leagueId, req.params.seasonId, matchupPeriod)
+        .then(data => {
+          points = squashPoints(data);
+          records = squashRecords(data);
+          breakdowns = squashBreakdowns(data);
+
+          rank(points);
+          rank(records);
+          rank(breakdowns);
+
+          ranks = new Array();
+          teamList.forEach(team => {
+            pointElement = jsonQuery("[teamId=" + team.teamId + "]", {
+              data: points
+            }).value;
+            recordElement = jsonQuery("[teamId=" + team.teamId + "]", {
+              data: records
+            }).value;
+            breakdownElement =
+              jsonQuery("[teamId=" + team.teamId + "]", {
+                data: breakdowns
+              }).value || {};
+            ranks.push({
+              name: team.name,
+              points: pointElement.display(),
+              pointsRank: points.length + 1 - pointElement.rank,
+              breakdown: breakdownElement.display(),
+              breakdownRank: points.length + 1 - breakdownElement.rank,
+              record: recordElement.display(),
+              recordRank: points.length + 1 - recordElement.rank,
+              powerPoints:
+                points.length +
+                1 -
+                pointElement.rank +
+                points.length +
+                1 -
+                breakdownElement.rank +
+                points.length +
+                1 -
+                recordElement.rank,
+              compare: function () {
+                //PowerPoints first with breakdown/points/record as tiebreaker
+                return this.powerPoints + ( this.breakdownRank / 10 ) + (this.pointsRank / 100 ) + (this.recordRank / 1000);
+              }
+            });
+          });
+
+          rank(ranks, false);
+          res.render("powerrank", { ranks: ranks });
+        });
+    })
+});
+
+function rank(data = {}, average=true) {
   allVals = new Array();
   data.forEach(element => {
     allVals.push(element.compare());
   });
-  allVals.sort(function(a, b) {
+  allVals.sort(function (a, b) {
     return a - b;
   });
   aCount = new Map(
     [...new Set(allVals)].map(x => [x, allVals.filter(y => y === x).length])
   );
   data.forEach(element => {
-    //Can we use aCount.value to average the position within indexOf?
     element.rank =
-      data.length -
-      (allVals.indexOf(element.compare()) +
-        (aCount.get(element.compare()) - 1));
+      data.length - ( average ? averageRank(
+      allVals.indexOf(element.compare()),
+        aCount.get(element.compare()))
+        : (allVals.indexOf(element.compare()) +
+        (aCount.get(element.compare()) - 1)) )
   });
-  data.sort(function(a, b) {
+  data.sort(function (a, b) {
     return a.rank - b.rank;
   });
 };
 
-generateUrl = function(leagueId, seasonId, matchupPeriodId) {
+function averageRank(starting, num) {
+  sum = 0;
+  for (let i = 0; i < num; i++) {
+    sum += (starting + i)
+  }
+  return sum / num;
+}
+
+function generateUrl(leagueId, seasonId, matchupPeriodId) {
   return (
     "http://games.espn.com/ffl/api/v2/scoreboard?leagueId=" +
     leagueId +
@@ -149,40 +139,94 @@ generateUrl = function(leagueId, seasonId, matchupPeriodId) {
   );
 };
 
-async function getBreakdowns(matchupPeriod, leagueId, seasonId, callback) {
-  const myArray = [];
+async function getData(leagueId, seasonId, matchupPeriod) {
+  const periods = [];
   for (let index = matchupPeriod; index > 0; index--) {
-    myArray.push(index);
+    periods.push(index);
   }
 
-  let finalArray = myArray.map(async value => {
-    // map instead of forEach
-    const result = await getSingleBreakdownPeriod(value, leagueId, seasonId);
-    finalValue.asyncFunctionValue = result.asyncFunctionValue;
-    return finalValue; // important to return the value
-  });
-  const resolvedFinalArray = await Promise.all(finalArray); // resolving all promises
-  return resolvedFinalArray;
+
+  const promises = periods.map(period =>
+    superagent.get(generateUrl(leagueId, seasonId, period))
+      .then(response => {
+        // if (err) {
+        //   console.log(err);
+        //   return;
+        // }
+
+        periodTeams = flattenMatchups( jsonQuery("scoreboard.matchups", {
+          data: response.body
+        }).value);
+
+        return { points: calculatePoints(periodTeams), records: calculateRecord(periodTeams), breakdowns: calculateBreakdown(periodTeams) };
+      }));
+
+  return await Promise.all(promises);
 }
 
-async function getSingleBreakdownPeriod(matchupPeriod, leagueId, seasonId) {
-  return superagent
-    .get(generateUrl(leagueId, seasonId, matchupPeriod))
-    .then((err, response) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
+function flattenMatchups(matchups) {
+  const teams = new Array();
+  matchups.forEach(matchup => {
+    matchup.teams.forEach(team => {
+      team.win = 0;
+      team.loss = 0;
+      team.tie = 0;
 
-      periodTeams = jsonQuery("scoreboard.matchups.teams", {
-        data: response.body
-      }).value;
-
-      return calculateBreakdown(teams);
+      if (matchup.winner === "home")
+        if (team.home)
+          team.win++;
+        else
+          team.loss++;
+      else if (matchup.winner === "away")
+        if (team.home)
+          team.loss++;
+        else
+          team.win++;
+      else
+        team.tie++;
+      teams.push(team);
     });
+  });
+  return teams;
 }
 
-calculateBreakdown = function(teams) {
+function calculatePoints(teams) {
+  let points = new Array();
+  teams.forEach(team => {
+    points.push({
+      teamId: team.teamId,
+      points: team.score,
+      compare: function () {
+        return this.points;
+      },
+      display: function () {
+        return this.points;
+      }
+    });
+  });
+  return points;
+}
+
+function calculateRecord(teams) {
+  let record = new Array();
+  teams.forEach(team => {
+    record.push({
+      teamId: team.teamId,
+      win: team.win,
+      loss: team.loss,
+      tie: team.tie,
+      compare: function () {
+        return this.win - this.loss;
+      },
+      display: function () {
+        return this.win + "-" + this.loss + "-" + this.tie;
+      }
+    });
+  });
+  return record;
+}
+
+function calculateBreakdown(teams) {
   let breakdown = new Array();
   teams.forEach(first => {
     let win = 0,
@@ -190,26 +234,69 @@ calculateBreakdown = function(teams) {
       tie = 0;
     teams.forEach(second => {
       if (first.teamId != second.teamId) {
-        if (first.score == second.score) tie++;
-        if (first.score > second.score) win++;
+        if (first.score === second.score) tie++;
+        else if (first.score > second.score) win++;
         else loss++;
       }
     });
     breakdown.push({
       teamId: first.teamId,
-      name: first.team.teamLocation + " " + first.team.teamNickname,
       win: win,
       loss: loss,
       tie: tie,
-      compare: function() {
+      compare: function () {
         return this.win - this.loss;
       },
-      display: function() {
+      display: function () {
         return this.win + "-" + this.loss + "-" + this.tie;
       }
     });
   });
   return breakdown;
 };
+
+
+function squashPoints(data) {
+  data[0].points.forEach(first => {
+    for (let index = 1; index < data.length; index++) {
+      data[index].points.forEach(second => {
+        if(first.teamId === second.teamId) {
+          first.points += second.points;
+        }
+      });
+    }
+  });
+  return data[0].points;
+}
+
+function squashRecords(data) {
+  data[0].records.forEach(first => {
+    for (let index = 1; index < data.length; index++) {
+      data[index].records.forEach(second => {
+        if(first.teamId === second.teamId) {
+          first.win += second.win;
+          first.loss += second.loss;
+          first.tie += second.tie;
+        }
+      });
+    }
+  });
+  return data[0].records;
+}
+
+function squashBreakdowns(data) {
+  data[0].breakdowns.forEach(first => {
+    for (let index = 1; index < data.length; index++) {
+      data[index].breakdowns.forEach(second => {
+        if(first.teamId === second.teamId) {
+          first.win += second.win;
+          first.loss += second.loss;
+          first.tie += second.tie;
+        }
+      });
+    }
+  });
+  return data[0].breakdowns;
+}
 
 module.exports = router;
